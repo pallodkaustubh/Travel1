07fdd1e75b488b91644e31631b2e045d62ea3ec8
Kaustubh Pallod <127481844+pallodkaustubh@users.noreply.github.com>
2024-03-27 16:00:19 +0530

    Update loginform.php

--- Admin/loginform.php
+++ Admin/loginform.php
@@ -11,46 +11,42 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--js--> 
 <script src="js/jquery.min.js"></script>
-
 <!--/js-->
 </head>
 <body>
 <!--header-->
 <!--sticky-->
-
 <?php include('function.php'); ?>
 <?php
 $_SESSION['loginstatus']="";
 if(isset($_POST["sbmt"]))
 {
-	$cn=makeconnection();
-	$s="select * from users where Username='" . $_POST["t1"] . "' and Pwd='" . $_POST["t2"] ."'";
-	
-	$q=mysqli_query($cn,$s);
-	$r=mysqli_num_rows($q);
-$data=mysqli_fetch_array($q);
-	mysqli_close($cn);
-	if($r>0)
-	{
-		$_SESSION["Username"]= $_POST["t1"];
-		$_SESSION["usertype"]=$data[2];
-		$_SESSION['loginstatus']="yes";
-		header("location:index.php");
-	}
-	else
-	{
-	echo "<script>alert('Invalid User Name or Password');</script>";
-}
+    $cn=makeconnection();
+    $username = mysqli_real_escape_string($cn, $_POST["t1"]); // Sanitize user input
+    $password = mysqli_real_escape_string($cn, $_POST["t2"]); // Sanitize user input
+    $s="SELECT * FROM users WHERE Username='$username' AND Pwd='$password'";
+    
+    $q=mysqli_query($cn,$s);
+    $r=mysqli_num_rows($q);
+    $data=mysqli_fetch_array($q);
+    mysqli_close($cn);
+    if($r>0)
+    {
+        $_SESSION["Username"]= $username;
+        $_SESSION["usertype"]=$data[2];
+        $_SESSION['loginstatus']="yes";
+        header("location:index.php");
+        exit; // Add exit to stop script execution after redirection
+    }
+    else
+    {
+        echo "<script>alert('Invalid User Name or Password');</script>";
+    }
 }
 ?>
-
-
 
 <?php include('topforlogin.php'); ?>
 <!--/sticky-->
@@ -59,30 +55,28 @@ $data=mysqli_fetch_array($q);
 
 </div>
 <div class="col-sm-9">
-
-
-
-
-<form method="post">
-<table border="0" width="500px" height="400px" align="left" class="tableshadow">
-<tr><td colspan="2" class="toptd"></td></tr>
-
-<tr><td></td>
-<td class="lefttxt"><table border="0" width="100px" height="200px"><td>User Name</td></td><td><input type="text" name="t1" required pattern="[a-zA-z _]{1,50}" title"Please Enter Only Characters between 1 to 50 for User Name" /></td></tr>
-<tr><td class="lefttxt">Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input type="password" name="t2" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters between 1 to 10 for Password" /></td></tr></table>
-<tr><td></td><td align="center" ><input type="submit" value="LOGIN" name="sbmt" /></td></tr>
-
-
-
-
-</table>
-</form>
-
-
-
+    <form method="post">
+        <table border="0" width="500px" height="400px" align="left" class="tableshadow">
+            <tr><td colspan="2" class="toptd"></td></tr>
+            <tr>
+                <td></td>
+                <td class="lefttxt">
+                    <table border="0" width="100px" height="200px">
+                        <tr>
+                            <td>User Name</td>
+                            <td><input type="text" name="t1" required pattern="[a-zA-z _]{1,50}" title="Please Enter Only Characters between 1 to 50 for User Name" /></td>
+                        </tr>
+                        <tr>
+                            <td class="lefttxt">Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                            <td><input type="password" name="t2" required pattern="[a-zA-z0-9]{1,10}" title="Please Enter Only Characters between 1 to 10 for Password" /></td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr><td></td><td align="center" ><input type="submit" value="LOGIN" name="sbmt" /></td></tr>
+        </table>
+    </form>
 </div>
-
-
 </div>
 <?php include('bottom.php'); ?>
 </body>
